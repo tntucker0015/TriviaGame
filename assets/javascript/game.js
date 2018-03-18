f8691ff52a9ef5d1517b3120763bc37bea70895a
Tim Tucker <tntucker0015@outlook.com>
2018-03-18 00:47:27 -0400

    initial commit for css, js

---
+++ assets/javascript/game.js
@@ -0,0 +1,53 @@
+
+// questions and answers for trivia game
+// Q. Which bird has the largest wingspan of any living bird?
+// A. The wandering albatross.
+// Q. True or false - the python is a poisonous snake?
+// A. False.
+// Q. What type of animal is a Mexican hairless?
+// A. A dog.
+// Q. What kind of creature is a Portuguese man o' war?
+// A. A jellyfish.
+// Q. How many legs does a lobster have?
+// A. 10.
+// Q. What type of animal is a Flemish giant?
+// A. A rabbit.
+// Q. Which creatures produce gossamer?
+// A. Spiders.
+// Q. What is a palmiped?
+// A. Any bird with webbed feet.
+// Q. From which animal is mohair obtained?
+// A. The angora goat.
+// Q. Whereabouts do demersal fish live in the sea - near the top, near the middle or near the bottom?
+// A. Near the bottom.
+// Q. How many arms do most starfish have?
+// A. Five.
+// Q. Which large mammal's tail is so strong it can stand on it and lift its hind legs off the ground?
+// A. The kangaroo.
+// Q. What is a cabbage white?
+// A. A butterfly.
+// Q. What is the natural habitat of arboreal animals?
+// A. In or amongst trees.
+// Q. What is the scientific name for the trunk of an elephant?
+// A. Proboscis.
+// Q. Which ape gets its name from the Malay word meaning "man of the forest"?
+// A. The orangutan.
+// Q. How is the domestic cavy better known?
+// A. As the guinea pig.
+// Q. What are the male honey bees called that are the only members of the colony allowed to mate with the queen?
+// A. Drones.
+// Q. Which adjective is used to describe a horse with two different colored patches, usually black and white?
+// A. Piebald.
+// Q. Which is the largest mammal in the world?
+// A. The blue whale.
+// Q. How many humps does a bactrian camel have?
+// A. Two.
+// Q. What type of animal is a basenji?
+// A. A hunting dog.
+// Q. Which is the largest living bird?
+// A. The ostrich.
+// Q. What kind of animal is a karakul?
+// A. A sheep.
+// Q. "Murder" is the collective noun for a group of which bird?
+// A. Crows.
+
