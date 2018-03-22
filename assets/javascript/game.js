c27410f594de56282bfa909491be2ec495299400
Tim Tucker <tntucker0015@outlook.com>
2018-03-22 15:36:05 -0400

    adding random questions and answers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,80 @@
+// to do list:
+// add your score area
+    //with correct answers
+    // with incorrect answers
+// add timer for time of each question:
+// add reset function
 
-// questions and answers for trivia game
-// Q. Which bird has the largest wingspan of any living bird?
-// A. The wandering albatross.
-// Q. True or false - the python is a poisonous snake?
-// A. False.
-// Q. What type of animal is a Mexican hairless?
-// A. A dog.
-// Q. What kind of creature is a Portuguese man o' war?
-// A. A jellyfish.
-// Q. How many legs does a lobster have?
-// A. 10.
-// Q. What type of animal is a Flemish giant?
-// A. A rabbit.
-// Q. Which creatures produce gossamer?
-// A. Spiders.
-// Q. What is a palmiped?
-// A. Any bird with webbed feet.
-// Q. From which animal is mohair obtained?
-// A. The angora goat.
-// Q. Whereabouts do demersal fish live in the sea - near the top, near the middle or near the bottom?
-// A. Near the bottom.
-// Q. How many arms do most starfish have?
-// A. Five.
-// Q. Which large mammal's tail is so strong it can stand on it and lift its hind legs off the ground?
-// A. The kangaroo.
-// Q. What is a cabbage white?
-// A. A butterfly.
-// Q. What is the natural habitat of arboreal animals?
-// A. In or amongst trees.
-// Q. What is the scientific name for the trunk of an elephant?
-// A. Proboscis.
-// Q. Which ape gets its name from the Malay word meaning "man of the forest"?
-// A. The orangutan.
-// Q. How is the domestic cavy better known?
-// A. As the guinea pig.
-// Q. What are the male honey bees called that are the only members of the colony allowed to mate with the queen?
-// A. Drones.
-// Q. Which adjective is used to describe a horse with two different colored patches, usually black and white?
-// A. Piebald.
-// Q. Which is the largest mammal in the world?
-// A. The blue whale.
-// Q. How many humps does a bactrian camel have?
-// A. Two.
-// Q. What type of animal is a basenji?
-// A. A hunting dog.
-// Q. Which is the largest living bird?
-// A. The ostrich.
-// Q. What kind of animal is a karakul?
-// A. A sheep.
-// Q. "Murder" is the collective noun for a group of which bird?
-// A. Crows.
+var currentQuestion;
+var game = {
+    yourScore: 0,
+    correctGuesses: 0,
+    incorrectGuesses: 0,
+}
 
+function getRandomQuestion() {
+  var random = Math.floor(Math.random() * quest.length);
+  return quest[random];
+}
+
+function getAnswers(question) {
+  var answers = [];
+  answers.push(currentQuestion.answer);
+
+  do {
+    // BUG:
+    var newQuestion = getRandomQuestion();
+    if (!answers.indexOf(newQuestion.answer) > -1) {
+      answers.push(newQuestion.answer);
+    } 
+  } while (answers.length < 4);
+  var random = Math.floor(Math.random() * 4)
+  var temp = answers[random];
+  answers[random] = answers[0];
+  answers[0] = temp;
+  return answers;
+}
+function sptAns () {
+    var x = (getAnswers(currentQuestion.answers));
+    console.log(x);
+    $("#answersA").text(x[0]);
+    $("#answersB").text(x[1]);
+    $("#answersC").text(x[2]);
+    $("#answersD").text(x[3]); 
+    console.log(x[0]);
+    console.log(x[1]);
+    console.log(x[2]);
+    console.log(x[3]);
+}
+
+function nextQuestion () {
+    currentQuestion = getRandomQuestion();
+    // console.log(currentQuestion);
+    // console.log(getAnswers(currentQuestion));
+    $("#triviaQuestion").text(currentQuestion.question);
+    // $("#answers").text(getAnswers(currentQuestion.answers));
+    // $("#answersA").text(getAnswers(currentQuestion.answers));
+    // $("#answersB").text(getAnswers(currentQuestion.answers));
+    // $("#answersC").text(getAnswers(currentQuestion.answers));
+    // $("#answersD").text(getAnswers(currentQuestion.answers));   
+    sptAns(); 
+}
+
+function startGame() {
+  currentQuestion = getRandomQuestion();
+  $("#triviaQuestion").text(currentQuestion.question);
+//   $("#answers").text(getAnswers(currentQuestion.answers));
+    $("#answersA").text(getAnswers(currentQuestion.answers));
+    $("#answersB").text(getAnswers(currentQuestion.answers));
+    $("#answersC").text(getAnswers(currentQuestion.answers));
+    $("#answersD").text(getAnswers(currentQuestion.answers));    
+
+}
+
+
+$("body").on("click", "#start", function () {
+    startGame();
+});
+$("body").on("click", "#next", function () {
+    nextQuestion();
+});
---
+++ assets/javascript/questions.js
@@ -0,0 +1,127 @@
+ var quest = [
+   
+    {
+        question: "Which bird has the largest wingspan of any living bird?",
+        answer:"The wandering albatross.",
+    },
+   
+    {
+        question:"Which is a non- poisonous snake?",
+        answer: "python",
+    },
+   
+    {
+        question: "What type of animal is a Mexican hairless?",
+        answer: "dog",
+    },
+
+    {
+        question: "What kind of creature is a Portuguese man o' war?",
+        answer: "jellyfish",
+    },
+   
+    {
+        question: "How animal has 10 legs?",
+        answer: "lobster",
+    },
+   
+    {
+        question: "What type of animal is a Flemish giant?",
+        answer: "rabbit",
+    },
+   
+    {
+        question: "Which creatures produce gossamer?",
+        answer: "Spiders",
+    },
+   
+    {
+        question: "What is a palmiped?",
+        answer: "Any bird with webbed feet",
+    },
+   
+    {
+        question: "From which animal is mohair obtained?",
+        answer: "The angora goat",
+    },
+   
+    {
+        question: "What fish live near the bottom of the sea?",
+        answer: "dermersal",
+    },
+   
+    {
+        question: "What animal has 5 arms?",
+        answer: "starfish",
+    },
+   
+    {
+        question: "Which large mammal's tail is so strong it can stand on it and lift its hind legs off the ground?",
+        answer: "The kangaroo",
+    },
+   
+    {
+        question: "What is a cabbage white?",
+        answer: "butterfly",
+    },
+   
+    {
+        question: "What type of animal has natural habitat in or among trees?", 
+        answer: "arboreal",
+    },
+   
+    {
+        question: "What animal has a trunk with the scientific name 'Proboscis?'", 
+        answer: "elephant",
+    },
+   
+    {
+        question: "Which ape gets its name from the Malay word meaning 'man of the forest'?",
+        answer: "orangutan",
+    },
+   
+    {
+        question: "How is the domestic cavy better known?",
+        answer: "guinea pig",
+    },
+   
+    {
+        question: "What is a Drone that is the only members of the colony allowed to mate with the queen?",
+        answer: "honey bee",
+    },
+   
+    {
+        question: "Piebald is an adjective is used to describe this animal with two different colored patches, usually black and white?",
+        answer: "Horse",
+    },
+   
+    {
+        question: "Which is the largest mammal in the world?",
+        answer: "blue whale",
+    },
+   
+    {
+        question: "This animal has two humps on (HUMP-DAY).",
+        answer: "bactrian camel",
+    },
+   
+    {
+        question: "What type of animal is a basenji?",
+        answer: "hunting dog",
+    },
+   
+    {    
+        question: "Which is the largest living bird?",
+        answer: "ostrich",
+    },
+   
+    {
+        question: "What kind of animal is a karakul?",
+        answer: "sheep",
+    },
+   
+    {
+        question: '"Murder" is the collective noun for a group of which bird?',
+        answer: "crows",
+    },
+ ]
